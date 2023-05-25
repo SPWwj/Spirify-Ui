4ca92d2e8d70d891bd4674ad2124367a86293e85
SPWwj <weiwenjie@u.nus.edu>
2023-05-26 02:56:51 +0800

    Fix Oauth state issue with password and username

--- src/pages/OAuthLoginPage.tsx
+++ src/pages/OAuthLoginPage.tsx
@@ -1,31 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { Form, Input, Button, message } from "antd";
-import authService, { OAuthRequestModel } from "services/AuthService";
+import authService from "services/AuthService";
 
 const OAuthLoginPage: React.FC = () => {
 	const [searchParams] = useSearchParams();
 
-	const [OAuthModel, setOAuthModel] = useState<OAuthRequestModel>({
-		username: "",
-		password: "",
-		responseType: searchParams.get("response_type") || "",
-		clientId: searchParams.get("client_id") || "",
-		redirectUri: searchParams.get("redirect_uri") || "",
-		scope: searchParams.get("scope") || "",
-	});
+
 
 	const handleSubmit = async (values: {
 		username: string;
 		password: string;
 	}) => {
 		try {
-			setOAuthModel((prevState) => ({
-				...prevState,
+			const OAuthModel = {
 				username: values.username,
 				password: values.password,
-			}));
-
+				responseType: searchParams.get('response_type') || '',
+				clientId: searchParams.get('client_id') || '',
+				redirectUri: searchParams.get('redirect_uri') || '',
+				scope: searchParams.get('scope') || '',
+			};
+	
 			await authService.oauthLogin(OAuthModel);
 		} catch (error) {
 			message.error("Login failed! Please check your username or password.");
