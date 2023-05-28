82b60e19fdf0f33f75f65e1b2f0cfb0c97e1ab21
SPWwj <weiwenjie@u.nus.edu>
2023-05-28 16:40:43 +0800

    Update metadata

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2,31 +2,30 @@ import React, { useContext, useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 import TokenService from "../authentication/TokenService";
 import { AuthContext } from "context/AuthContext";
+import { Typography, Button } from 'antd';
+import './ProfilePage.module.scss';
+
+const { Title, Paragraph } = Typography;
 
 const ProfilePage: React.FC = () => {
 	const [email, setEmail] = useState("");
-	const [usernameCookie, setUsernameCookie] = useState("");
 	const { username, setUsername, logout } = useContext(AuthContext)!;
-	const storedUsername = TokenService.getCurrentUser();
 
 	useEffect(() => {
 		const token = TokenService.getAccessToken();
 		if (token) {
 			const decodedToken: any = jwt_decode(token);
-			setUsernameCookie(decodedToken.sub);
 			setEmail(decodedToken.email);
 		}
 	}, [setUsername]);
 
 	return (
-		<div>
-			<h1>Profile</h1>
-			<p>Username: {username}</p>
-			<p>storedUsername: {storedUsername}</p>
-			<p>usernameCookie: {usernameCookie}</p>
-			<p>Email: {email}</p>
-			<button onClick={() => setUsername(username + 1)}>Increment</button>
-			<button onClick={logout}>Logout</button>
+		<div className="profile-page">
+			<Title>Profile</Title>
+			<Paragraph>Username: {username}</Paragraph>
+			<Paragraph>Email: {email}</Paragraph>
+			<Button onClick={() => setUsername(username + 1)} type="primary">Increment</Button>
+			<Button onClick={logout} danger >Logout</Button>
 		</div>
 	);
 };
