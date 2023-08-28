6851473d4fe7ed80c3df9ae6900f5c47f5cf6140
SPWwj <wei_dev@outlook.com>
2023-08-28 13:13:36 +0800

    Add layout

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,49 +1,26 @@
-import { Card as CustomCard } from "components/cards/Card";
-import { Card } from "antd";
-import React from "react";
-import Meta from "antd/es/card/Meta";
+import { Card as CustomCard } from "components/cards/Card"; // Your custom Card import
+import { Row, Col } from "antd"; // Import Row and Col from antd
 
 const About: React.FC = () => {
+	// Generate an array of 20 CustomCard components
+	const customCardSamples = Array.from({ length: 20 }, (_, index) => (
+		<Col key={index} xs={12} sm={12} md={8} lg={8} xl={6}>
+			<CustomCard
+				background="black"
+				title="Hello World!"
+				textColor="white"
+				icons={["fa fa-heart", "fa fa-comment", "fa fa-share-alt", "fa fa-trash"]}
+				backgroundImage="https://spirify.azurewebsites.net/icon-512.png"
+			/>
+		</Col>
+	));
+
 	return (
 		<div className="page page--about">
-			{" "}
-			<i className="fa fa-heart" aria-hidden="true"></i> Click the heart!
-			<i className="fa fa-heart" aria-hidden="true"></i> Click the heart!
-			<h2>About Page</h2>
-			<CustomCard
-				textColor="black"
-				icons={["fa fa-heart", "fa fa-clipboard"]}
-			/>
-			<Card
-				hoverable
-				style={{ width: 240 }}
-				cover={
-					<img
-						alt="example"
-						src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-					/>
-				}
-			>
-				{/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
-			</Card>
-			<CustomCard
-				background="#3C3B3D"
-				textColor="white"
-				title="Hello World!"
-				icons={["fa fa-heart", "fa fa-comment", "fa fa-share-alt"]}
-			/>
-			<CustomCard
-				background="#5D9CEC"
-				title="Hello World!"
-				textColor="white"
-				icons={[
-					"fa fa-heart",
-					"fa fa-comment",
-					"fa fa-share-alt",
-					"fa fa-trash",
-				]}
-				backgroundImage="https://spirify.azurewebsites.net/icon-512.png"
-			/>{" "}
+			{/* existing code */}
+			<Row gutter={[16, 16]} justify="center">
+				{customCardSamples}
+			</Row>
 		</div>
 	);
 };
